96489c84b7ec4fbd694ae747a7e74a01d3f04e6e
guptamansi18 <mansi.gupta@creolestudios.com>
2021-11-11 10:19:39 +0530

    saga

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cartReducer";
 import ProductReducer from "./ProductReducer";
+import counter from "./counter";
 
 const rootReducer = combineReducers({
     ProductReducer,
-    cartReducer
+    cartReducer,
+    counter
 })  
 
 export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,17 @@
 import { createStore } from "redux";
+import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from "redux-devtools-extension";
 import { applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
 import rootReducer from "./reducers/RootReducer";
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
+import { helloSaga } from './saga'
 
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+
+sagaMiddleware.run(helloSaga)
+
+// const action = type => store.dispatch({type})
 export default store;
