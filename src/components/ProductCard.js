99dcca23cee742ec2bc0d911c3ed22bcd8f99f1a
gupta-mansi <gupta.mansijp@gmail.com>
2021-11-17 14:43:23 +0530

    search-bar

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd';
 import { Card } from 'antd';
 import { Layout, Menu } from 'antd';
 import { useSelector } from 'react-redux';
+import { loadCache } from 'axios-hooks';
 
 const ProductCard = () => {
+    
+const [SearchKey, setSearchKey] = useState('');
 
 const { Content } = Layout;
 
 const ProductsData = useSelector(state => state.products.ProductData);
 
+
+const FilteredProducts = (SearchKey.length > 0 ? ProductsData.filter(data => {
+    return data.title.toLowerCase().includes(SearchKey.toLowerCase())}) : '' )
+
 console.log('ProductData',ProductsData);
 
     return (
         <div>
+            <center>   
+        <input type="text" placeholder="Search here...." value={SearchKey} onChange={(e) => setSearchKey(e.target.value)} style={{ marginTop: '15px', width: '450px', height: '40px' }}/>
+        </center>
             <div style={{display:'flex', flexWrap:'wrap', padding: '5px'}}> 
+            {
+                FilteredProducts?.length > 0 ? 
+                FilteredProducts.map(data => (
+                        <Content style={{padding: '5px'}}>
+                            <Card hoverable bordered size="small" title={data.title} style={{ width: 300, height: 350, background: 'lightblue' }}>
+                                <center> <img src={data.image} width="150px" height="150px" alt="product image" style={{margin:'2px' , padding:'2px' }} ></img></center>
+                                <p style={{ maxHeight: 45, height: '20%', overflow: 'hidden' }}>{data.description}</p>
+                            <Button type="primary" size="large" key={data.id} style={{ width: 270, marginBottom: '5px' }} >Add to Cart</Button>
+                            </Card>
+                        </Content>
+                        ))
 
-            {ProductsData.length > 0 ? 
+                    : 
+                    
+            
+            ProductsData.length > 0 ? 
                 ProductsData.map(data => (
                     <Content style={{padding: '5px'}}>
                         <Card hoverable bordered size="small" title={data.title} style={{ width: 300, height: 350, background: 'lightblue' }}>
@@ -25,7 +49,7 @@ console.log('ProductData',ProductsData);
                         <Button type="primary" size="large" key={data.id} style={{ width: 270, marginBottom: '5px' }} >Add to Cart</Button>
                         </Card>
                     </Content>
-                    ))
+                    ))  
                     : 
                     
                     '' 
